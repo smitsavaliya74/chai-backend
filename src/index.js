8870036c5d99491d04d721eb45dffeb17456c8df
smitsavaliya74 <smitsavaliya74@gmail.com>
2026-05-30 00:51:47 +0530

    connect to MongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+
+
+
+
+
+
+/*
+
+import express from "express";
+
+const app = express()
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error", (error) => {
+            console.log("Error: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listing on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+*/
