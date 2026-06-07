a15d65d91337124eb229771802c327626ab34f15
smitsavaliya74 <smitsavaliya74@gmail.com>
2026-06-08 00:14:46 +0530

    middleware and cloudinarysetup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -80,7 +80,7 @@ userSchema.methods.generateAccessToken = async function () {
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullname = this.fullname
+            fullname : this.fullname
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
